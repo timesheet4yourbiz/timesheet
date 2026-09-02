76bf9293f20ecd83ee9345c941a7b2cad369ae27
timesheet4yourbiz <timesheet4yourbiz@gmail.com>
2026-09-02 09:14:47 +0800

    Update timesheet.js

--- js/timesheet.js
+++ js/timesheet.js
@@ -1,6 +1,7 @@
 import { supabase } from './supabase.js';
-
+import { loadSidebar } from './sidebar.js';
 document.addEventListener('DOMContentLoaded', async () => {
+loadSidebar(); 
     
     // Auth & Init
     const { data: { session } } = await supabase.auth.getSession();
