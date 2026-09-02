dd307249471a4efff2d45ec44fad4cf6f11d8a39
timesheet4yourbiz <timesheet4yourbiz@gmail.com>
2026-09-02 09:46:05 +0800

    Update departments.js

--- js/departments.js
+++ js/departments.js
@@ -1,13 +1,17 @@
 import { supabase } from './supabase.js';
 import { loadSidebar } from './sidebar.js';
 document.addEventListener('DOMContentLoaded', async () => {
-loadSidebar(); 
-    
-    // Semak Session
+loadSidebar();    
+
+    // Auth
     const { data: { session } } = await supabase.auth.getSession();
-    if (!session) {
-        window.location.href = '../pages/login.html';
-        return;
+    if (!session) return window.location.href = '../pages/login.html';
+    const logoutBtn = document.getElementById('logoutBtn');
+    if (logoutBtn) {
+    logoutBtn.addEventListener('click', async () => {
+            await supabase.auth.signOut();
+            window.location.href = '../pages/login.html';
+        });
     }
     
     document.getElementById('userEmail').textContent = session.user.email;
